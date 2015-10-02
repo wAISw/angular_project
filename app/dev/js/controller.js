e58edf051386d7da644074f67fb0aff824d63c85
ais <abdulov001@gmail.com>
2015-10-03 00:29:27 +0600

    Разобрался с фабриками, сервисами, провайдерами

--- app/dev/js/controller.js
+++ app/dev/js/controller.js
@@ -7,14 +7,23 @@
             'trackerApp.personal',
             'ngAnimate'
         ])
+        .constant('FIRE_BASE', 'http')
+        .value('configOptions', {
+            lang: 'ru',
+            timezone: '+3'
+        })
         .config(config)
+        .run(Run)
         .controller('UsersCtrl', UsersCtrl)
+        .factory('UsersFactory', UsersFactory)
+        .service('UsersService', UsersService)
+        .provider('Users', UsersProvider)
         .filter("formatCurrency", formatCurrency);
 
     /* Controllers */
 
 // @ngInject
-    function config($stateProvider, $urlRouterProvider, $locationProvider) {
+    function config($stateProvider, $urlRouterProvider, $locationProvider, UsersProvider, $provide) {
         $urlRouterProvider.otherwise('/');
         $stateProvider
             .state('About', {
@@ -33,22 +42,94 @@
                 url: '/contact',
                 templateUrl: 'partials/partial-contacts.html'
             });
+        console.log(UsersProvider);
+        UsersProvider.setPrivate('Not Almost Private');
+        console.log(UsersProvider);
+
+        $provide.decorator('UsersFactory', ['$delegate', function ($delegate) {
+            $delegate.helloPrivate = function () {
+                return "hello " + $delegate.getPrivate();
+            };
+            return $delegate;
+        }]);
         //$locationProvider.html5Mode(true);
     }
 
 // @ngInject
-    function UsersCtrl($scope, $http) {
+    function UsersService() {
+        var privateVal = null;
+
+        this.val = "some value";
+
+        this.getPrivate = function () {
+            return privateVal;
+        };
+
+        this.setPrivate = function (_val) {
+            privateVal = _val;
+        };
+    }
+
+// @ngInject
+    function UsersProvider(){
+        var privateVal = "private";
+        return {
+            setPrivate: function (_privateVal) {
+                privateVal = _privateVal;
+            },
+            $get: function () {
+                var o = {};
+                o.getPrivate = function () {
+                    return privateVal;
+                };
+
+                return o;
+            }
+        };
+    }
+// @ngInject
+    function UsersFactory() {
+        var o = {};
+
+        var privateVal = null;
+
+        o.val = "some value";
+
+        o.getPrivate = function () {
+            return privateVal;
+        };
+
+        o.setPrivate = function (_val) {
+            privateVal = _val;
+        };
+
+        return o;
+    }
+
+// @ngInject
+    function Run(configOptions, UsersFactory, UsersService) {
+        console.log(configOptions);
+        UsersFactory.setPrivate('hello');
+        console.log(UsersService.getPrivate());
+        UsersService.setPrivate("opa")
+    }
+
+// @ngInject
+    function UsersCtrl($scope, $http, UsersFactory, UsersService, Users) {
         var s = this;
         s.title = "Пользователи";
+        s.hello = UsersFactory.helloPrivate();
+        console.log(UsersService.getPrivate());
+        console.log(Users.getPrivate());
         $http.get('db/users.json').success(function (data) {
             s.users = data;
         });
     }
 
 // @ngInject
-    function formatCurrency () {
+    function formatCurrency() {
         return function (val) {
-            return new Intl.NumberFormat('ru-RU', { style: 'currency', currency: 'RUB' }).format(val);
+            return new Intl.NumberFormat('ru-RU', {style: 'currency', currency: 'RUB'}).format(val);
         }
     }
 
--- app/dev/personal/personal.js
+++ app/dev/personal/personal.js
@@ -19,25 +19,25 @@
                 controller: 'PersonalIndexCtrl',
                 controllerAs: '_pic_'
             })
-             .state('Complex', {
+            .state('Complex', {
                 url: '/personal/complex',
                 templateUrl: 'personal/partial-complex.html',
                 controller: 'PersonalComplexCtrl',
                 controllerAs: '_pcc_'
             })
-             .state('Settings', {
+            .state('Settings', {
                 url: '/personal/settings',
                 templateUrl: 'personal/partial-settings.html',
                 controller: 'PersonalSettingsCtrl',
                 controllerAs: '_psc_'
             })
-             .state('Statistics', {
+            .state('Statistics', {
                 url: '/personal/statistics',
                 templateUrl: 'personal/partial-statistics.html',
                 controller: 'PersonalStatisticsCtrl',
                 controllerAs: '_pstatc_'
             })
-              .state('Workout', {
+            .state('Workout', {
                 url: '/personal/workout',
                 templateUrl: 'personal/partial-workout.html',
                 controller: 'PersonalWorkoutCtrl',
@@ -46,35 +46,65 @@
     }
 
     // @ngInject
-    function PersonalIndexCtrl (){
+    function PersonalIndexCtrl() {
         var s = this;
         s.title = "Личный кабинет";
     }
 
     // @ngInject
-    function PersonalComplexCtrl (){
+    function PersonalComplexCtrl() {
         var s = this;
         s.title = "Мои комплексы";
     }
 
     // @ngInject
-    function PersonalSettingsCtrl (){
+    function PersonalSettingsCtrl() {
         var s = this;
         s.title = "Мои настройки";
     }
 
     // @ngInject
-    function PersonalStatisticsCtrl (){
+    function PersonalStatisticsCtrl() {
         var s = this;
         s.title = "Моя статистика";
     }
 
     // @ngInject
-    function PersonalWorkoutCtrl (){
+    function PersonalWorkoutCtrl() {
         var s = this;
         s.title = "Тренировка";
     }
-
-
-
 })();
+
+var UserRepository = function () {
+    // private
+    var repository;
+
+    function createRepository() {
+        repository = {
+            count: 2
+        };
+        return repository;
+    }
+
+    return {
+        getInstance: function () {
+            if (!repository) {
+                console.log("create");
+                repository = createRepository();
+            }
+            return repository;
+        }
+    };
+};
+
+var repository = new UserRepository();
+var rep1 = repository.getInstance();
+var rep2 = repository.getInstance();
+console.log(rep1.count, rep2.count);
+
+rep1.count = 1;
+console.log(rep1.count, rep2.count);
+
+rep2.count = 3;
+console.log(rep1.count, rep2.count);
